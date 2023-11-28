34a5d07e2f59056cb9b9a56b9db4fdd54554c035
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2023-11-29 00:31:07 +0600

    console app deploy

---
+++ 4.deploy_asp.net_core_console _app/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hi {name}.");
+        Console.ReadLine();
+    }
+}
